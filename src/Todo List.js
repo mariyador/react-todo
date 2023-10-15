5eb2faf28ca5f8dea55ea680c3e49df51df6b142
Mariya <mariya@MacBook-Air-Mariya.local>
2023-10-15 12:58:11 -0400

    Move List Item into New 'Todo List Item' Component

--- src/Todo List.js
+++ src/Todo List.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoListItem from './TodoListItem';
 
 let todoList = [
     {id: 1, title: "Complete assignment"},
@@ -10,7 +11,7 @@ function TodoList() {
     return(
         <ul>
             {todoList.map((item) => (
-            <li key={item.id}>{item.title}</li>
+                <TodoListItem key={item.id} todo={item} />
             ))}
         </ul>
     )
