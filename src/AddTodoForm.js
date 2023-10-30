d7e66b00a5dd13d2a1722ce034ff0ace9f8824ef
Mariya <mariya@MacBook-Air-Mariya.local>
2023-10-29 23:23:36 -0400

    Add New Todo to List

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 function AddTodoForm(props) {
-    let handleAddTodo = (event) => {
+    const [todoTitle, setTodoTitle] = React.useState('');
+
+    const handleTitleChange = (event) => {
+        const newTodoTitle = event.target.value;
+        setTodoTitle(newTodoTitle);
+    }
+
+    const handleAddTodo = (event) => {
         event.preventDefault();
-        let todoTitle = event.target.elements.title.value;
         console.log(todoTitle);
         event.target.reset();
         props.onAddTodo(todoTitle);
@@ -11,7 +17,7 @@ function AddTodoForm(props) {
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoList">Title </label>
-            <input type="text" id="todoList" name='title'></input>
+            <input type="text" id="todoList" name='title' value={todoTitle} onChange={handleTitleChange}></input>
             <button type="submit">Add</button>
         </form>
     )
