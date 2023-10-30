f509fd5376c7e555bb086dda9adcaa86b687ce4d
Mariya <mariya@MacBook-Air-Mariya.local>
2023-10-29 23:02:45 -0400

    Move Todo List into State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import AddTodoForm from './AddTodoForm';
-import TodoList from './Todo List';
+import TodoList from './TodoList';
 
 
 function App() {
   const [newTodo, setNewTodo] = React.useState('');
+  const [todoList, setTodoList] = React.useState([]);
+
   return (
     <div>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={setNewTodo} />
       <p>{newTodo}</p>
-      <TodoList />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
+
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-let todoList = [
-    {id: 1, title: "Complete assignment"},
-    {id: 2, title: "Practice more"},
-    {id: 3, title: "Have a rest"},
-];
-
-function TodoList() {
+function TodoList(props) {
     return(
         <ul>
-            {todoList.map((item) => (
+            {props.todoList.map((item) => (
                 <TodoListItem key={item.id} todo={item} />
             ))}
         </ul>
