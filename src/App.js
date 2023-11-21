1b5992f09db290891df55ba9950d67d8a01a33e7
Mariya <mariya@MacBook-Air-Mariya.local>
2023-11-20 20:03:29 -0500

    Async

--- src/App.js
+++ src/App.js
@@ -5,12 +5,27 @@ import TodoList from './TodoList';
 
 function App() {
 
-  const savedTodoList = JSON.parse(localStorage.getItem('savedTodoList')) || [];
-  const [todoList, setTodoList] = React.useState(savedTodoList);
+  const [todoList, setTodoList] = React.useState([]);
 
   React.useEffect(() => {
-    localStorage.setItem('savedTodoList',JSON.stringify(todoList));
+    const savedTodoList = JSON.parse(localStorage.getItem('savedTodoList')) || [];
+    setTodoList(savedTodoList)
+  }, []);
+
+  React.useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
   }, [todoList]);
+
+  React.useEffect(() => {
+    const myPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ data: { todoList: [] } });
+      }, 2000)    
+    });
+    myPromise.then((result) => {
+      setTodoList(result.data.todoList);
+    })
+}, []);
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
